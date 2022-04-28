ee4da596cd80b2141b1d7781ce638ed64ad4a95b
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-28 06:58:29 -0500

    Activity.jsx file fetching data properly and displaying to the webpage

---
+++ never-be-bored/src/components/Activity/Activity.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import "./Activity.css";
+
+const url = "https://www.boredapi.com/api/activity";
+
+const Activity = () => {
+    const [activity, setActivity] = useState("");
+
+    const FetchActivity = async () => {
+        try {
+        const response = await fetch(url);
+        const jsonData = await response.json();
+        console.log(jsonData);
+        setActivity(jsonData);
+        } catch (err) {
+        console.error(err);
+        }
+    };
+
+    const handleGetActivity = (e) => {
+        FetchActivity();
+    };
+
+    useEffect(() => {
+        FetchActivity();
+    }, []);
+    return (
+        <>
+        <div
+            className={
+            activity.type !== "undefined"
+                ? activity.type === "education"
+                ? "app education"
+                : activity.type === "relaxation"
+                ? "app relaxation"
+                : activity.type === "busywork"
+                ? "app busywork"
+                : activity.type === "recreational"
+                ? "app recreational"
+                : activity.type === "music"
+                ? "app music"
+                : activity.type === "charity"
+                ? "app charity"
+                : activity.type === "diy"
+                ? "app diy"
+                : activity.type === "social"
+                ? "app social"
+                : activity.type === "cooking"
+                ? "app cooking"
+                : "app"
+                : "app"
+            }
+        >
+            <main>
+            {/* <h1 className="title">Do something!</h1> */}
+            <button className="activityBtn" onClick={handleGetActivity}>
+                Get Activity
+            </button>
+            <div className="activityContainer">
+                <p className="activityContent">{activity.activity}</p>
+            </div>
+            </main>
+        </div>
+        </>
+    );
+};
+
+export default Activity;
